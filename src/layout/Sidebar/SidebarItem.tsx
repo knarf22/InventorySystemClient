3bbc85bd192605a94244a09f3090c8bb026c2690
knarf22 <gonzalesjosefrank22@gmail.com>
2025-12-03 22:32:27 +0800

    shet

--- src/layout/Sidebar/SidebarItem.tsx
+++ src/layout/Sidebar/SidebarItem.tsx
@@ -9,7 +9,6 @@ const SidebarItem = ({
   children,
   isOpen,
   hasChildren,
-  
 }: SidebarItemProps) => {
   return (
     <div>
@@ -18,24 +17,41 @@ const SidebarItem = ({
         className={`flex items-center justify-between px-4 py-3 rounded-lg cursor-pointer transition-all duration-300
           ${
             active
-              ? "bg-gradient-to-r from-purple-700 to-purple-600  shadow-md"
+              ? "bg-white text-purple-600 shadow-md"
               : "text-muted hover:bg-gradient-to-r hover:from-sidebarHover hover:to-purple-800 hover:text-secondary"
           }`}
       >
         <div className="flex items-center gap-3">
-          <div className="w-5 h-5">{icon}</div>
-          <span className="text-sm font-medium">{label}</span>
+          <div
+            className={`w-5 h-5 ${
+              active ? "text-purple-600" : "text-muted"
+            }`}
+          >
+            {icon}
+          </div>
+          <span
+            className={`text-sm font-medium ${
+              active ? "text-purple-600" : ""
+            }`}
+          >
+            {label}
+          </span>
         </div>
 
         {hasChildren && (
           <ChevronRight
             size={16}
             className={`transition-transform duration-300 ${
-              isOpen ? "rotate-90 text-secondary" : "text-muted"
+              isOpen
+                ? "rotate-90 text-purple-600"
+                : active
+                ? "text-purple-600"
+                : "text-muted"
             }`}
           />
         )}
       </div>
+
       {children}
     </div>
   );
--- src/layout/Sidebar/SidebarSubitem.tsx
+++ src/layout/Sidebar/SidebarSubitem.tsx
@@ -1,18 +1,17 @@
 import type { SidebarSubItemProps } from "../../types/ISideBar";
 
-
 const SidebarSubItem = ({ icon, label, active, onClick }: SidebarSubItemProps) => (
   <div
     onClick={onClick}
     className={`flex items-center gap-2 px-3 py-2 rounded-md cursor-pointer transition-colors duration-200
       ${
         active
-          ? "bg-sidebarHover text-600 font-medium"
+          ? "bg-white text-purple-600 font-medium"
           : "text-muted hover:bg-sidebarHover hover:text-secondary"
       }`}
   >
-    {icon}
-    <span>{label}</span>
+    <div className={`${active ? "text-purple-600" : ""}`}>{icon}</div>
+    <span className={active ? "text-purple-600" : ""}>{label}</span>
   </div>
 );
 
